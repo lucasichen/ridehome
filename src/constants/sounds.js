40631fdcdc4689ab6f227355e5531bf1b212effe
Lucas Chen <lucas.chen2016@gmail.com>
2022-06-04 21:41:01 -0400

    add audio urls to dropdown

--- src/constants/sounds.js
+++ src/constants/sounds.js
@@ -1,13 +1,20 @@
 // Import and export sounds
 
-import subway from '../assets/subway.m4a'; 
+import endless from '../assets/subway.m4a'; 
 import announcer from '../assets/announcer.mp3';
 import talking from '../assets/talking.mp3';
 import music from '../assets/music.mp3';
-// import test from 'https://ridehome.s3.amazonaws.com/sounds/talking.mp3';
+const line1 = 'https://ridehome.s3.amazonaws.com/sounds/line1.mp3';
+const line2 = 'https://ridehome.s3.amazonaws.com/sounds/line2.mp3';
+const line3 = 'https://ridehome.s3.amazonaws.com/sounds/line3.mp3';
+const line4 = 'https://ridehome.s3.amazonaws.com/sounds/line4.mp3';
 
 export default {
-    subway,
+    endless,
     talking,
     music,
+    line1,
+    line2,
+    line3,
+    line4,
 };
