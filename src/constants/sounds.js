2a89c95c6431cc9cad9e453576c89ae2ee07c90d
Lucas Chen <71472753+lucasichen@users.noreply.github.com>
2022-06-06 08:51:20 -0400

    Update sound file routes

--- src/constants/sounds.js
+++ src/constants/sounds.js
@@ -1,12 +1,12 @@
 // Import and export sounds
 
-const endless = 'https://ridehome.s3.amazonaws.com/sounds/subway.m4a';
-const talking = 'https://ridehome.s3.amazonaws.com/sounds/talking.mp3';
-const music = 'https://ridehome.s3.amazonaws.com/sounds/music.mp3';
-const line1 = 'https://ridehome.s3.amazonaws.com/sounds/line1.mp3';
-const line2 = 'https://ridehome.s3.amazonaws.com/sounds/line2.mp3';
-const line3 = 'https://ridehome.s3.amazonaws.com/sounds/line3.mp3';
-const line4 = 'https://ridehome.s3.amazonaws.com/sounds/line4.mp3';
+const endless = 'https://ridehome.s3.amazonaws.com/sounds/endless.m4a';
+const talking = 'https://ridehome.s3.amazonaws.com/talking/talking.mp3';
+const music = 'https://ridehome.s3.amazonaws.com/music/music.mp3';
+const line1 = 'https://ridehome.s3.amazonaws.com/sounds/TTC+Line+1.mp3';
+const line2 = 'https://ridehome.s3.amazonaws.com/sounds/TTC+Line+2.mp3';
+const line3 = 'https://ridehome.s3.amazonaws.com/sounds/TTC+Line+3.mp3';
+const line4 = 'https://ridehome.s3.amazonaws.com/sounds/TTC+Line+4.mp3';
 
 export default {
     endless,
