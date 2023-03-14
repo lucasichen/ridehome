75c6edaecd4f2f41af4532c1b8c5dcf036e731b2
Lucas Chen <lucas.chen2016@gmail.com>
2023-03-14 17:54:50 -0400

    remove react test files and libraries

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,6 @@
 // Import and export images
-
-// import train from '../assets/train_inside.gif';
 import train_night from '../assets/train_night.gif';
 
 export default {
-    // train,
     train_night,
 };
--- src/constants/sounds.js
+++ src/constants/sounds.js
@@ -3,18 +3,9 @@ import endless from '../assets/endless.m4a';
 import music from '../assets/music.mp3';
 import talking from '../assets/talking.mp3';
 
-const line1 = 'https://ridehome.s3.amazonaws.com/sounds/TTC+Line+1.mp3';
-const line2 = 'https://ridehome.s3.amazonaws.com/sounds/TTC+Line+2.mp3';
-const line3 = 'https://ridehome.s3.amazonaws.com/sounds/TTC+Line+3.mp3';
-const line4 = 'https://ridehome.s3.amazonaws.com/sounds/TTC+Line+4.mp3';
-
 
 export default {
     endless,
     talking,
     music,
-    line1,
-    line2,
-    line3,
-    line4,
 };
